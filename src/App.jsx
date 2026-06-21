f674e63024a65edf120087f694a62d4265c9f278
ParthaPraneeth01 <parthapraneeth01@gmail.com>
2026-06-21 22:49:32 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import ReferralDetail from './pages/ReferralDetail'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/referral/:id"
+          element={
+            <ProtectedRoute>
+              <ReferralDetail />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/dashboard/referrals" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
